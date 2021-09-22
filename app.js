013787f09e7b3b2618c79b0ae1573138dee0a6a8
edudifermo <edifermo@gmail.com>
2021-09-22 20:28:55 -0300

    cambio en hoja de estilos

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app = express();
 const path = require('path')
 
-app.use(express.static( path.join(__dirname, '/public')))
+//app.use(express.static( path.join(__dirname, '/public')))
+
+app.use("/public",express.static(path.join(__dirname,"public")));
 
 app.get('/', (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
